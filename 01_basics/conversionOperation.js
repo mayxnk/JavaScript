97f87218221146ee083bbbd23334e84efe18d410
Mayank Rastogi <105148831+mayxnk@users.noreply.github.com>
2023-10-10 09:47:54 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33abc";
+
+console.log(typeof score);
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1;
+let booleanIsLOggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLOggedIn);
