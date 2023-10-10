f0e3e71836a36a8c49bee10707d0e9924abe0528
Mayank Rastogi <105148831+mayxnk@users.noreply.github.com>
2023-10-10 10:25:08 +0000

    updated

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc";
+/*let score = "33abc";
 
 console.log(typeof score);
 let valueInNumber = Number(score);
@@ -9,3 +9,26 @@ console.log(valueInNumber);
 let isLoggedIn = 1;
 let booleanIsLOggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLOggedIn);
+
+*/
+
+// ***************** Operations *****************
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+let str1 = "Hello "
+let str2 = "Mayank"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);//122
+
+console.log(1 + 2 + "2");//32
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
