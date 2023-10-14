159edfde4e915a3a569924f3174a793f17b2f054
Mayank Rastogi <105148831+mayxnk@users.noreply.github.com>
2023-10-14 17:10:46 +0000

    02 basics folder created

---
+++ 02_basics/arrays.js
@@ -0,0 +1,20 @@
+//array
+
+const arr = [0,1,2,3,4,5]
+const heroes = ["shaktiman","naagraj"]
+
+const arr2 = new Array(1,2,3,4)
+
+console.log(arr2);
+
+//Array Methods
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+
+// arr.unshift(9)
+//arr.shift()
+console.log(arr);
+
+console.log(arr.includes(10));
+console.log(arr.indexOf(3));
