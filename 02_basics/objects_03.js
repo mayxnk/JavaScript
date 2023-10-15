7416fc68cc5e1f3ec65d2a75fc74f344391da3c7
Mayank Rastogi <105148831+mayxnk@users.noreply.github.com>
2023-10-15 15:31:30 +0000

    object_03.js created

---
+++ 02_basics/objects_03.js
@@ -0,0 +1,11 @@
+const course = {
+    courseName:"Js in Hindi",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+const {courseInstructor: instructore} = course
+console.log(instructore);
+
+// const navbar = () => {
+
+// }
