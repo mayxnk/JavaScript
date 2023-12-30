15d649abaa0d3f324b0b44e5b532df6b690c398b
Mayank Rastogi <105148831+mayxnk@users.noreply.github.com>
2023-12-30 07:13:51 +0000

    filter_map updated

--- 03_basics/filter_map_and_reduce.js
+++ 03_basics/filter_map_and_reduce.js
@@ -4,3 +4,13 @@
 // const newNums = myNums.filter((num) => num > 4)
 // console.log(newNums);
 
+//Reduce
+
+myNums = [1,2,3]
+// const myTotal = myNums.reduce(function(acc, currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// },0)
+const myTotal = myNums.reduce((acc,currval) => (acc + currval),0)
+
+console.log(myTotal);
